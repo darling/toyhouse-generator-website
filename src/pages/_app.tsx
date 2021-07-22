1558b2629e70f5a0112bbdea6a424f6c22a35fee
Carter Black <carterb@mailbox.org>
2021-07-22 16:56:47 -0700

    font awesome changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../styles/site_bootstrap.css'
 import '../styles/fa-all.min.css'
 import './../styles/main.css'
+// import './../styles/th-main.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
